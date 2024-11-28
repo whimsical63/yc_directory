90f1aa9baf8fe3b16fc915a7ad21900b30001983
L A <laurenceunabia@gmail.com>
2024-11-28 10:29:28 +0800

    Update View.tsx

--- components/View.tsx
+++ components/View.tsx
@@ -4,18 +4,43 @@ import { STARTUP_VIEWS_QUERY } from "@/sanity/lib/queries";
 import { writeClient } from "@/sanity/lib/write-client";
 import { unstable_after as after } from "next/server";
 
+// const View = async ({ id }: { id: string }) => {
+//   const { views: totalViews } = await client
+//     .withConfig({ useCdn: false })
+//     .fetch(STARTUP_VIEWS_QUERY, { id });
+//
+//   after(
+//     async () =>
+//       await writeClient
+//         .patch(id)
+//         .set({ views: totalViews + 1 })
+//         .commit(),
+//   );
+//
+//   return (
+//     <div className="view-container">
+//       <div className="absolute -top-2 -right-2">
+//         <Ping />
+//       </div>
+//
+//       <p className="view-text">
+//         <span className="font-black">Views: {totalViews}</span>
+//       </p>
+//     </div>
+//   );
+// };
+// export default View;
+
 const View = async ({ id }: { id: string }) => {
   const { views: totalViews } = await client
     .withConfig({ useCdn: false })
     .fetch(STARTUP_VIEWS_QUERY, { id });
 
-  after(
-    async () =>
-      await writeClient
-        .patch(id)
-        .set({ views: totalViews + 1 })
-        .commit(),
-  );
+  // Update the views in a single transaction
+  await writeClient
+    .transaction()
+    .patch(id, (patch) => patch.inc({ views: 1 })) // Increment the view count atomically
+    .commit();
 
   return (
     <div className="view-container">
@@ -24,7 +49,7 @@ const View = async ({ id }: { id: string }) => {
       </div>
 
       <p className="view-text">
-        <span className="font-black">Views: {totalViews}</span>
+        <span className="font-black">Views: {totalViews + 1}</span>
       </p>
     </div>
   );
